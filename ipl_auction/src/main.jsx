b53605a540599e48a674f0b9af4b0db8d5e8b059
RonitKorat07 <ronitkorat7@gmail.com>
2025-02-25 12:29:20 +0530

    create auction page
    auction page design

--- ipl_auction/src/main.jsx
+++ ipl_auction/src/main.jsx
@@ -14,6 +14,7 @@ import Footer from './components/footer.jsx'
 import { store } from './store/store.js'
 import { Provider } from 'react-redux'
 import PlayerProfile from './pages/playerprofilepage.jsx'
+import Auction from './pages/auction.jsx'
 
 
 const router = createBrowserRouter([
@@ -45,6 +46,11 @@ const router = createBrowserRouter([
       {
         path: '/playerprofile/:id',
         element: <><PlayerProfile /><Footer /></>
+      }
+      ,
+      {
+        path: '/auction',
+        element: <><Auction /><Footer /></>
       }
     ]
   }
